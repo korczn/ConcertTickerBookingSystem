68ec79298c13e75dab067728cb0528f1325c0cc7
maciejs13 <maciej.sledzinski@cyfroweszkoly.pl>
2024-12-22 21:33:49 +0100

    klasa Concert z wlasciwosciami (nazwa, data, lokalizacja, dostepne miejsca) i konstruktorem inicjalizujacym

---
+++ ConcertTickerBookingSystem/Concert.cs
@@ -0,0 +1,18 @@
+namespace ConcertTicketBooking
+{
+    public class Concert
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public int AvailableSeats { get; set; }
+        
+        public Concert(string name, DateTime date, string location, int availableSeats)
+        {
+            Name = name;
+            Date = date;
+            Location = location;
+            AvailableSeats = availableSeats;
+        }
+    }
+}
